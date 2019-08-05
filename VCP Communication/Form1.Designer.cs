a37f5ae154bc7acd2446d275d62b3d5e7724c7b5
platonovsergey <sergey.platonov98@gmail.com>
2019-08-05 18:42:10 +0300

    Initial commit

---
+++ VCP Communication/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace VCP_Communication
+{
+    partial class VCP_Communication
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbPort = new System.Windows.Forms.ComboBox();
+            this.btnConnection = new System.Windows.Forms.Button();
+            this.txtMsg = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.cbStopBits = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cbParity = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cbBoudRate = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this._serialPort = new System.IO.Ports.SerialPort(this.components);
+            this.console = new System.Windows.Forms.RichTextBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "COM Port";
+            // 
+            // cbPort
+            // 
+            this.cbPort.FormattingEnabled = true;
+            this.cbPort.Location = new System.Drawing.Point(71, 18);
+            this.cbPort.Name = "cbPort";
+            this.cbPort.Size = new System.Drawing.Size(121, 21);
+            this.cbPort.TabIndex = 2;
+            // 
+            // btnConnection
+            // 
+            this.btnConnection.Location = new System.Drawing.Point(198, 17);
+            this.btnConnection.Name = "btnConnection";
+            this.btnConnection.Size = new System.Drawing.Size(75, 23);
+            this.btnConnection.TabIndex = 3;
+            this.btnConnection.Text = "Connect";
+            this.btnConnection.UseVisualStyleBackColor = true;
+            this.btnConnection.Click += new System.EventHandler(this.BtnConnection_Click);
+            // 
+            // txtMsg
+            // 
+            this.txtMsg.Location = new System.Drawing.Point(15, 45);
+            this.txtMsg.Name = "txtMsg";
+            this.txtMsg.Size = new System.Drawing.Size(177, 20);
+            this.txtMsg.TabIndex = 4;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(198, 43);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 5;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.BtnSend_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.cbStopBits);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.cbParity);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.cbBoudRate);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(289, 18);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(167, 116);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Settings";
+            // 
+            // cbStopBits
+            // 
+            this.cbStopBits.FormattingEnabled = true;
+            this.cbStopBits.Location = new System.Drawing.Point(70, 75);
+            this.cbStopBits.Name = "cbStopBits";
+            this.cbStopBits.Size = new System.Drawing.Size(86, 21);
+            this.cbStopBits.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 78);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Stop Bits";
+            // 
+            // cbParity
+            // 
+            this.cbParity.FormattingEnabled = true;
+            this.cbParity.Location = new System.Drawing.Point(70, 48);
+            this.cbParity.Name = "cbParity";
+            this.cbParity.Size = new System.Drawing.Size(86, 21);
+            this.cbParity.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 51);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Parity";
+            // 
+            // cbBoudRate
+            // 
+            this.cbBoudRate.FormattingEnabled = true;
+            this.cbBoudRate.Location = new System.Drawing.Point(70, 22);
+            this.cbBoudRate.Name = "cbBoudRate";
+            this.cbBoudRate.Size = new System.Drawing.Size(86, 21);
+            this.cbBoudRate.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Boud Rate";
+            // 
+            // console
+            // 
+            this.console.Location = new System.Drawing.Point(15, 71);
+            this.console.Name = "console";
+            this.console.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.console.Size = new System.Drawing.Size(258, 147);
+            this.console.TabIndex = 12;
+            this.console.Text = "";
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(15, 224);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 13;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.BtnClear_Click);
+            // 
+            // VCP_Communication
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(470, 336);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.console);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtMsg);
+            this.Controls.Add(this.btnConnection);
+            this.Controls.Add(this.cbPort);
+            this.Controls.Add(this.label1);
+            this.Name = "VCP_Communication";
+            this.Text = "VCP_Communication";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbPort;
+        private System.Windows.Forms.Button btnConnection;
+        private System.Windows.Forms.TextBox txtMsg;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox cbBoudRate;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cbStopBits;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cbParity;
+        private System.Windows.Forms.Label label3;
+        private System.IO.Ports.SerialPort _serialPort;
+        private System.Windows.Forms.RichTextBox console;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ VCP Communication/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VCP_Communication
+{
+    public partial class VCP_Communication : Form
+    {
+        static bool btnIsClicked;
+        Vcp vcp;
+
+        public VCP_Communication()
+        {
+            InitializeComponent();
+            vcp = new Vcp();
+        }
+
+        public void PortChat_Init()
+        {
+            _serialPort.PortName = cbPort.Text;
+            _serialPort.BaudRate = int.Parse(cbBoudRate.Text);
+            _serialPort.Parity = (Parity)Enum.Parse(typeof(Parity), cbParity.Text);
+            _serialPort.StopBits = (StopBits)Enum.Parse(typeof(StopBits), cbStopBits.Text);
+        }
+
+        public static void SetTextUI(RichTextBox rtb, string data)
+        {
+            if (rtb.InvokeRequired)
+            {
+                Action putInText = () =>
+                {
+                    rtb.AppendText(data);
+                    rtb.SelectionStart = rtb.Text.Length;
+                };
+
+                rtb.Invoke(putInText);
+            }
+            else
+            {
+                rtb.AppendText(data);
+                rtb.SelectionStart = rtb.Text.Length;
+            }
+        }
+
+        private void PortChat_Listen(object sender, SerialDataReceivedEventArgs e)
+        {
+            string data;
+            data = "Received: " + _serialPort.ReadExisting();
+
+            SetTextUI(console, data);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btnIsClicked = false;
+
+            cbPort.Items.AddRange(vcp.portsName);
+
+            foreach (int item in vcp.boudRate)
+            {
+                cbBoudRate.Items.Add(item);
+            }
+
+            foreach (KeyValuePair<Parity, String> item in vcp.parity)
+            {
+                cbParity.Items.Add(item.Key);
+            }
+
+            foreach (KeyValuePair<StopBits, string> item in vcp.stopBits)
+            {
+                cbStopBits.Items.Add(item.Key);
+            }
+
+            cbPort.SelectedIndex = cbPort.Items.Count - 1;
+            cbBoudRate.SelectedIndex = 4;
+            cbParity.SelectedIndex = 0; 
+            cbStopBits.SelectedIndex = 1;
+
+            PortChat_Init();
+        }
+
+        private void BtnConnection_Click(object sender, EventArgs e)
+        {
+            if (btnIsClicked == false)
+            {
+                _serialPort.Open();
+                _serialPort.DataReceived += new SerialDataReceivedEventHandler(PortChat_Listen);
+                btnIsClicked = true;
+                console.AppendText("Connected to " + _serialPort.PortName + ", " + _serialPort.BaudRate + ", " + _serialPort.Parity + ", " + _serialPort.StopBits + "\n");
+                btnConnection.Text = "Disconnect";
+            }
+            else
+            {
+                _serialPort.Close();
+                btnIsClicked = false;
+                console.AppendText("Disconnected to " + _serialPort.PortName + "\n");
+                btnConnection.Text = "Connect";
+            }
+        }
+
+        private void BtnSend_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (_serialPort.IsOpen)
+                {
+                    //_serialPort.WriteLine(console.Text + Environment.NewLine);
+                }
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void BtnClear_Click(object sender, EventArgs e)
+        {
+            if (_serialPort.IsOpen)
+            {
+                console.Text = "Connected to " + _serialPort.PortName + ", " + _serialPort.BaudRate + ", " + _serialPort.Parity + ", " + _serialPort.StopBits + Environment.NewLine;
+            }
+            else
+            {
+                console.Text = "";
+            }
+        }
+    }
+
+    public class Vcp
+    {
+        public string[] portsName = SerialPort.GetPortNames();
+        public List<int> boudRate = new List<int>();
+        public Dictionary<StopBits, String> stopBits = new Dictionary<StopBits, string>();
+        public Dictionary<Parity, String> parity = new Dictionary<Parity, string>();
+
+        public Vcp()
+        {
+            boudRate.Add(600);
+            boudRate.Add(1200);
+            boudRate.Add(2400);
+            boudRate.Add(4800);
+            boudRate.Add(9600);
+            boudRate.Add(14400);
+            boudRate.Add(19200);
+            boudRate.Add(28800);
+            parity.Add(Parity.None, "None (Нет)");
+            parity.Add(Parity.Odd, "Odd (Нечётность)");
+            parity.Add(Parity.Even, "Even (Нечётность)");
+            parity.Add(Parity.Mark, "Mark (Единица)");
+            parity.Add(Parity.Space, "Space (Ноль)");
+            stopBits.Add(StopBits.None, "None (0)");
+            stopBits.Add(StopBits.One, "One (1)");
+            stopBits.Add(StopBits.Two, "Two (2)");
+            stopBits.Add(StopBits.OnePointFive, "OnePointFive (1,5)");
+        }
+    }
+}
